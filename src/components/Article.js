2ab6994f94c14e56d75f0d479d49ddaf31715d15
Didi <ofisser86@gmail.com>
2020-05-29 16:56:17 +0300

    state-lifting

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,112 +1,67 @@
-import React, { Component } from 'react'
+import React, {PureComponent} from 'react'
 
+class Article extends PureComponent {
+    constructor(props) {
+        super(props)
 
-class Article extends Component {
-    // constructor(props){
-    //     super(props)
-
-    //     this.state = {
-    //         isOpen: true
-
-    //     }
-    //        this.handleClick = handleClick.bind(this)
-
-    // }
-    // Experimental syntax
-    state = {
-
-        isOpen: this.props.defaultOpen,
-        count: 0
+        this.state = {
+            count: 0
+        }
     }
 
-    shouldComponentUpdate(nextProps, nextState){
+/*
+    shouldComponentUpdate(nextProps, nextState) {
         return this.state.isOpen !== nextState.isOpen
-
-
     }
-    componentWillMount(){
-        console.log("---", 'mounting')
+*/
 
-    }
-
-    componentWillReceiveProps(nextProps){
-        console.log("---", 'will recieve')
-        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState(
-            {
-                isOpen:nextProps.defaultOpen
-            }
-        )
+    componentWillMount() {
+        console.log('---', 'mounting')
     }
 
-    componentWillUpdate(){
-        console.log("---", 'will update')
+/*
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+            isOpen: nextProps.defaultOpen
+        })
+    }
+*/
+
+    componentWillUpdate() {
+        console.log('---', 'will update')
     }
 
     render() {
-        const { article } = this.props
-        console.log('---', this.props)
-        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+        const {article, isOpen, onButtonClick} = this.props
+        const style = {width: '50%'}
+        const body = isOpen && <section className="card-text">{article.text}</section>
         return (
-            <div className="card mx-auto"  style={{width:'75%'}}>
+            <div className="card mx-auto" style = {style}>
                 <div className="card-header">
-                    <h2 onClick={this.incrementCounter}>
+                    <h2 onClick = {this.incrementCounter}>
                         {article.title}
                         clicked {this.state.count}
-                        <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>
-                            {this.state.isOpen ? 'close' : 'open'}
+                        <button onClick={onButtonClick} className="btn btn-primary btn-lg float-right">
+                            {isOpen ? 'close' : 'open'}
                         </button>
                     </h2>
                 </div>
                 <div className="card-body">
                     <h6 className="card-subtitle text-muted">
-                        Creation date {(new Date(article.date)).toDateString()}
+                        creation date: {(new Date(article.date)).toDateString()}
                     </h6>
-                    
                     {body}
-
                 </div>
             </div>
         )
-
-
     }
-    handleClick = () => {
-        console.log('----', 'cliked')
+
+    incrementCounter = () => {
         this.setState({
-            isOpen: !this.state.isOpen
-
+            count: this.state.count + 1
         })
-        }
-
-        incrementCounter = () => {
-            this.setState({
-                count: this.state.count + 1
-            })
-        }
-    //===================Basic React component with restrict functionality=====================
-    // function Article(props){
-    //     const {article} = props
-    //     const foo = props.foo
-    //     console.log('---', props)
-    //     const body = <section>{article.text}</section>
-    //     return (
-    //         <div>
-    //         <h2>
-    //             {article.title}
-    //             <button onClick={handleClick}>close</button>    
-    //         </h2>
-    //         {body}
-    //         <p>Date is { (new Date(article.date)).toDateString() } </p>
-    //     <p>{foo}</p>
-
-    //         </div>
-    //     )
-    //     }
-
-    //=======Old syntax======
-    // function handleClick() {
-    //     console.log('----', 'cliked')
-
-    // }
+    }
 }
+
+
 export default Article
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import Article from '../Article'
 import './style.css'
 
-
-export default function ArticleList({articles}){
-    const articalElement = articles.map((articl, index) => 
-        <li key= {articl.id}><Article article={articl} defaultOpen = {index === 0} /></li>
+export default class ArticleList extends PureComponent {
+    state = {
+        openArticleId: null
+    }
+    render() {
+        console.log('---', 2)
+        const articleElements = this.props.articles.map((article, index) =>
+            <li key = {article.id} className="article-list__li">
+                <Article article = {article}
+                         isOpen = {this.state.openArticleId === article.id}
+                         onButtonClick = {this.handleClick.bind(this, article.id)}
+                />
+            </li>
         )
-    return(
+        return (
+            <ul>
+                {articleElements}
+            </ul>
+        )
+    }
 
-       <ul className="article-list_li">
-            {articalElement}
-       </ul> 
-    )
-
-
+    handleClick = openArticleId => this.setState({
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+    })
 }
